23d5d509f8e6fb69bf65ee87077f11fe4f2a06c1
shu <shuguoli68@163.com>
2019-12-02 17:32:25 +0800

    修改好导航

--- lib/widget/main/search_page.dart
+++ lib/widget/main/search_page.dart
@@ -1,10 +1,12 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:enjoy_android/entity/navigate_entity.dart';
 import 'package:enjoy_android/global/api_service.dart';
+import 'package:enjoy_android/global/common.dart';
 import 'package:flutter/material.dart';
 import 'package:zeking_refresh/zeking_refresh.dart';
 
 import '../../entity_factory.dart';
+import '../web_widget.dart';
 
 
 class SearchPage extends StatefulWidget {
@@ -30,8 +32,6 @@ class _SearchPageState extends State<SearchPage> {
       NavigateEntity entity = EntityFactory.generateOBJ(json);
       if(entity.errorCode == 0){//成功
         setState(() {
-          BotToast.showText(text: entity.errorMsg);
-          data.clear();
           data.addAll(entity.mData);
         });
         if(entity.mData.isNotEmpty)
@@ -57,7 +57,41 @@ class _SearchPageState extends State<SearchPage> {
       child: ListView.builder(
         padding: EdgeInsets.all(10),
         itemBuilder: (BuildContext context, int index) {
-          return Text(data[index].name);
+          return Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(5),
+                margin: EdgeInsets.only(top: 30),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black12, width: 2),
+                  borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                ),
+                child: Text(data[index].name),
+              ),
+              ListView.builder(
+                shrinkWrap: true, //解决无限高度问题
+                physics:NeverScrollableScrollPhysics(),//禁用滑动事件
+                padding: EdgeInsets.all(10),
+                itemBuilder: (BuildContext context, int subIndex) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        border: Border(
+                            bottom: Divider.createBorderSide(context,color: Colors.grey,width: 1)
+                        )
+                    ),
+                    child: ListTile(
+                      onTap: (){
+                        goTo(context, WebWidget(url: data[index].articles[subIndex].link, title: data[index].articles[subIndex].title,));
+                      },
+                      title: Text(data[index].articles[subIndex].title),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ),
+                  );
+                },
+                itemCount: data[index].articles.length,
+              ),
+            ],
+          );
         },
         itemCount: data.length,
       ),
